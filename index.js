bf06bdf7ee6bfe03ec2ba79556807fa36f9a3c84
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-10-18 23:43:41 -0400

    tdhjsfgjxdfhz

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-
-let color = [];
-let colors = {
-    color1: 'red',
-    color2: 'blue',
-    color3: 'green',
-    color: ["white", "silver", "grey", "black"]
+student1 = {
+	name: "person 1",
+	id: 1,
+	grade: 11,
+	classes: ["math", "science"],
 };
 
-let carMakes = [];
-let makes = {
-    make1: 'BMW',
-    make2: 'Mercedes',
-    make3: 'Alfa Romeo',
-    carMakes: ["Bugatti", "Pagani", "Lamborghini", "Ferarri"]
+student2 = {
+	name: "person 2",
+	id: 2,
+	grade: 10,
+	classes: ["math", "science", "history", "english"],
 };
 
-let cities = [];
-let countries = {
-    country1: 'Switzerland',
-    country2: 'Finland',
-    country3: 'Iceland',
-    cities: ["Reykjavík", "Dublin", "Oslo", "Tokyo"]
+student3 = {
+	name: "person 3",
+	id: 3,
+	grade: 9,
+	classes: ["science", "history"],
 };
 
-let objectArray = [colors, makes, countries];
+student4 = {
+	name: "person 4",
+	id: 4,
+	grade: 12,
+	classes: ["math", "science", "history"],
+};
 
-console.log(countries.country1);
+students = [student1, student2, student3, student4];
 
-objectArray.forEach(objectArray => {
-    console.log();
+// Part 1
+console.log('Part 1');
+students.forEach((s) => {
+    console.log(s.name);
+});
+
+// Part 2
+console.log('Part 2');
+students.forEach((s) => {
+    console.log(s.classes);
+});
+
+// Part 3
+console.log('Part 3');
+filteredStudents = students.filter((s) => {
+    return s.grade > 10;
+});
+
+filteredStudents.forEach((s) => {
+	console.log(s);
 });
